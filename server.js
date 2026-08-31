60f00312878d7e355b44f56300680debf9e75d14
manojtech-dev <manojmohan.0620@gmail.com>
2026-08-31 08:29:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const SECRET_KEY = 'mysecretkey';
 let db;
@@ -69,4 +72,10 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
